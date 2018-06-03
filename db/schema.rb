d44c79a312f2877de155d37db0b7876dc1d1cfad
Nur Sanwari <nur.sanwari@fdmgroup.com>
2018-06-03 13:10:19 +0800

    fix search home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602234746) do
+ActiveRecord::Schema.define(version: 20180603045058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180602234746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "img_url"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["location_id"], name: "index_products_on_location_id"
     t.index ["user_id"], name: "index_products_on_user_id"
